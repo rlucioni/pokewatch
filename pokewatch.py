c37d7c0d063d4d90820061312b93af44c9e22553
Renzo Lucioni <renzo@renzolucioni.com>
2016-07-26 21:47:50 -0400

    Add basic Pokewatcher utility class

--- pokewatch.py
+++ pokewatch.py
@@ -1,15 +1,54 @@
+import json
+import datetime
+
 import requests
 
-coordinates = ('42.364399098341', '-71.102836263782')
+
+CENTRAL = ('42.364399098341', '-71.102836263782')
 
 
-def build_url(coordinates):
-    return 'https://pokevision.com/map/data/{}/{}'.format(*coordinates)
+class Pokewatcher:
+    pokedex_path = 'pokedex.json'
+    data_url = 'https://pokevision.com/map/data/{}/{}'
 
+    def __init__(self, coordinates):
+        self.coordinates = coordinates
 
-def get_data(coordinates):
-    url = build_url(coordinates)
-    # TODO: Timeout and other error handling.
-    response = requests.get(url)
+        self.url = self.build_url()
+        self.pokedex = self.load_pokedex()
+        self.nearby = self.load_nearby()
 
-    return response.json()
+    def build_url(self):
+        return self.data_url.format(*self.coordinates)
+
+    def load_pokedex(self):
+        with open(self.pokedex_path) as f:
+            data = json.loads(f.read())
+
+        return {int(k): v for k, v in data.items()}
+
+    def load_nearby(self):
+        # TODO: Timeout and other error handling. Log status.
+        response = requests.get(self.url)
+        data = response.json()
+
+        return data['pokemon']
+
+    def expires_in(self, timestamp):
+        # TODO: Use Arrow?
+        expiration = datetime.datetime.fromtimestamp(timestamp)
+        now = datetime.datetime.now()
+
+        return expiration - now
+
+    def lookup(self, pokemon):
+        expires_in = self.expires_in(pokemon['expiration_time'])
+        name = self.pokedex[pokemon['pokemonId']]
+
+        return name, str(expires_in)
+
+    def refresh(self):
+        self.nearby = self.load_nearby()
+
+    def view(self):
+        return [self.lookup(pokemon) for pokemon in self.nearby]
