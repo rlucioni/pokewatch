410de4bf8c2ef088c3c9dd274e9591074238e1c0
Renzo Lucioni <renzo@renzolucioni.com>
2016-07-27 00:11:22 -0400

    Refine and document Pokewatcher
    Includes class used to represent Pokemon.

--- pokewatch.py
+++ pokewatch.py
@@ -1,5 +1,6 @@
-import json
 import datetime
+import itertools
+import json
 
 import requests
 
@@ -8,6 +9,7 @@ CENTRAL = ('42.364399098341', '-71.102836263782')
 
 
 class Pokewatcher:
+    """Utility for monitoring PokeVision."""
     pokedex_path = 'pokedex.json'
     data_url = 'https://pokevision.com/map/data/{}/{}'
 
@@ -19,36 +21,67 @@ class Pokewatcher:
         self.nearby = self.load_nearby()
 
     def build_url(self):
+        """
+        Using the provided coordinates, construct a URL that can be used to request
+        data from the PokeVision server.
+        """
         return self.data_url.format(*self.coordinates)
 
     def load_pokedex(self):
+        """Load the Pokedex JSON from disk."""
         with open(self.pokedex_path) as f:
             data = json.loads(f.read())
 
         return {int(k): v for k, v in data.items()}
 
     def load_nearby(self):
+        """Load nearby Pokemon data from the PokeVision server."""
         # TODO: Timeout and other error handling. Log status.
         response = requests.get(self.url)
         data = response.json()
 
         return data['pokemon']
 
-    def expires_in(self, timestamp):
+    def refresh_nearby(self):
+        """Update nearby Pokemon data by making a new request to the PokeVision server."""
+        self.nearby = self.load_nearby()
+
+    def nearby_pokemon(self):
+        """
+        Return a list of nearby Pokemon.
+
+        The list of nearby Pokemon returned by the PokeVision server often
+        includes several elements representing the same Pokemon. The expiration
+        times on these elements are separated by less than a second. Conveniently,
+        these elements seem to always be adjacent to each other in the list.
+
+        itertools' groupby() is used here to fold those adjacent elements together.
+        We then run with the first of the grouped elements, which seems to always
+        be the element expiring soonest.
+
+        Note: To fully deduplicate the nearby list, sort it by Pokemon ID first
+        (and optionally, by expiration time).
+        """
+        pokemon = []
+        for key, group in itertools.groupby(self.nearby, key=lambda p: p['pokemonId']):
+            data = next(group)
+            pokemon.append(
+                Pokemon(self.pokedex[data['pokemonId']], data['expiration_time'])
+            )
+
+        return pokemon
+
+
+class Pokemon:
+    """Representation of a Pokemon."""
+    def __init__(self, name, expiration):
+        self.name = name
+        self.expiration = expiration
+
+    def expires_in(self):
+        """Calculate time remaining until this Pokemon disappears from the map."""
         # TODO: Use Arrow?
-        expiration = datetime.datetime.fromtimestamp(timestamp)
+        expiration = datetime.datetime.fromtimestamp(self.expiration)
         now = datetime.datetime.now()
 
         return expiration - now
-
-    def lookup(self, pokemon):
-        expires_in = self.expires_in(pokemon['expiration_time'])
-        name = self.pokedex[pokemon['pokemonId']]
-
-        return name, str(expires_in)
-
-    def refresh(self):
-        self.nearby = self.load_nearby()
-
-    def view(self):
-        return [self.lookup(pokemon) for pokemon in self.nearby]
